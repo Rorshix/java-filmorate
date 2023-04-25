f61fe576b60a104f84d1a753e07ef816272056ae
Иван <v.chester@yandex.ru>
2023-04-25 17:01:09 +0300

    Тест4

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class InMemoryFilmStorage implements FilmStorage {
 
 	private final Map<Integer, Film> films = new HashMap<>();
-	private Integer globalId ;
+	private Integer globalId;
 
 	@Override
 	public List<Film> getAllFilms() {
