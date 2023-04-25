0c8d9603607ac4623e3d8c996c692275eae00c9d
Иван <v.chester@yandex.ru>
2023-04-25 11:59:44 +0300

    Тест4

--- src/main/java/ru/yandex/practicum/filmorate/localDate/LocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/localDate/LocalDateValidator.java
@@ -7,16 +7,15 @@ import java.time.Month;
 
 public class LocalDateValidator implements
 		ConstraintValidator<LocalDateRestrictions, LocalDate> {
-	private static final LocalDate CREATED_MOVIE_DATE = LocalDate.of ( 1895, Month.DECEMBER, 27 );
-
+	private static final LocalDate CREATED_MOVIE_DATE = LocalDate.of(1895, Month.DECEMBER, 27);
 
 	@Override
 	public void initialize(LocalDateRestrictions constraintAnnotation) {
-		ConstraintValidator.super.initialize ( constraintAnnotation );
+		ConstraintValidator.super.initialize(constraintAnnotation);
 	}
 
 	@Override
 	public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
-		return date != null && date.isAfter ( CREATED_MOVIE_DATE );
+		return date != null&&date.isAfter(CREATED_MOVIE_DATE);
 	}
 }
