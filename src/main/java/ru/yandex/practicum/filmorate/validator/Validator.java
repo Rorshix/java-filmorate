7c74e4e8207a617fd1c2445860d1368e7d86b448
Иван <v.chester@yandex.ru>
2023-04-26 17:55:59 +0300

    10500 Новый поворот+Valid

--- src/main/java/ru/yandex/practicum/filmorate/validator/Validator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/Validator.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.validator;
 
 import org.springframework.http.HttpStatus;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.exception.ValidationExceptionHttp;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
