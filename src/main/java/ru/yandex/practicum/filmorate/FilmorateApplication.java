bc058931ebfcb7993546297193c5a9430044287e
Ivan <v.chester@yandex.ru>
2023-06-11 01:14:40 +0300

    Ураааа

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,14 +2,9 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//надкжда
+
 @SpringBootApplication
 public class FilmorateApplication {
-
-    
-    
-
-
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
     }
