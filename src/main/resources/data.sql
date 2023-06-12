3967526e63358a47f700d483ac2164e9a86233fa
Ivan <v.chester@yandex.ru>
2023-06-12 15:25:29 +0300

    Chek rev

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,13 @@
-INSERT INTO status (Status_name) values('Approved');INSERT INTO status (Status_name) values('Not approved');INSERT INTO rating (rating_name) values('G');INSERT INTO rating (rating_name) values('PG');INSERT INTO rating (rating_name) values('PG-13');INSERT INTO rating (rating_name) values('R');INSERT INTO rating (rating_name) values('NC-17');INSERT INTO genre (genre_name) values('Комедия');INSERT INTO genre (genre_name) values('Драма');INSERT INTO genre (genre_name) values('Мультфильм');INSERT INTO genre (genre_name) values('Триллер');INSERT INTO genre (genre_name) values('Документальный');INSERT INTO genre (genre_name) values('Боевик');
+INSERT INTO status (Status_name) values('Approved');
+INSERT INTO status (Status_name) values('Not approved');
+INSERT INTO rating (rating_name) values('G');
+INSERT INTO rating (rating_name) values('PG');
+INSERT INTO rating (rating_name) values('PG-13');
+INSERT INTO rating (rating_name) values('R');
+INSERT INTO rating (rating_name) values('NC-17');
+INSERT INTO genre (genre_name) values('Комедия');
+INSERT INTO genre (genre_name) values('Драма');
+INSERT INTO genre (genre_name) values('Мультфильм');
+INSERT INTO genre (genre_name) values('Триллер');
+INSERT INTO genre (genre_name) values('Документальный');
+INSERT INTO genre (genre_name) values('Боевик');
